271060eb9145b63bd6ce88f93a63deb242301081
John D Pell <John+bashd@gaelicWizard.net>
2013-10-24 15:56:08 -0700

    Disable most environment variables.
    These variables are badly prone to getting out of date and are generally not needed much anymore due to better defaults and smarter tools.

--- bash/Xcode.env.bash
+++ bash/Xcode.env.bash
@@ -6,43 +6,44 @@
 if [ "`uname -s`" == "Darwin" ]
     # If we're on Mac OS X:
 then
-    export MACOSX_DEPLOYMENT_TARGET="10.$((`uname -r | awk -F . '{print $1}'`-4))" 
+:
+#    export MACOSX_DEPLOYMENT_TARGET="10.$((`uname -r | awk -F . '{print $1}'`-4))" 
         # Use the full feature set of the linker in the version of Darwin I'm running!
         # All system packages were compiled with this, and some local packages must be compiled with the matching setting.
-    export IPHONEOS_DEPLOYMENT_TARGET="3.1.3" # The last version to run on the original iPhone, up from default "2.0".
+#    export IPHONEOS_DEPLOYMENT_TARGET="3.1.3" # The last version to run on the original iPhone, up from default "2.0".
 
     # Make sure that any searching knows where to search.
-    export DEVELOPER_DIR="$(xcode-select -print-path 2>/dev/null)"
+#    export DEVELOPER_DIR="$(xcode-select -print-path 2>/dev/null)"
         # xcode-select determines which relocatable Xcode owns the non-relocatable "UNIX" tools.
         # $DEVELOPER_DIR overrides xcode-select's setting, which we want since we're _partially_ overriding it next:
-    export DEVELOPER_USR_DIR="${DEVELOPER_DIR:=/Developer}/usr"
+#    export DEVELOPER_USR_DIR="${DEVELOPER_DIR:=/Developer}/usr"
 #    export GCC_EXEC_PREFIX="${DEVELOPER_DIR}/usr/lib/gcc/" # trailing slash
         # Use the compiler _inside_ the Xcode root.
         # For some unknown reason, GCC is compiled with --prefix=/usr (but seemingly with --libdir /lib, and _something_ makes it want to search /usr/lib/../../libexec/gcc or some such.), even though it is housed inside the relocatable /Developer hierarchy. 
-    export SDKROOT="${DEVELOPER_DIR}/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk"
+#    export SDKROOT="${DEVELOPER_DIR}/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk"
         # Use the most-recent SDK.
 #    export CPPFLAGS+="${CPPFLAGS:+ }${SDKROOT:+-isysroot }${SDKROOT:-}"
 #    export  LDFLAGS+="${LDFLAGS:+ }${SDKROOT:+-isysroot }${SDKROOT:-}"
         # Apple's docs say to use -isysroot instead of --sysroot.
 
-    export ARCHS="$(ls /usr/libexec/gcc/darwin 2>/dev/null)"
+#    export ARCHS="$(ls /usr/libexec/gcc/darwin 2>/dev/null)"
         # Universal. 
             # 64-bit PowerPC support is lacking, and only Mac OS X 10.5 ever supported 64-bit Cocoa on PowerPC.
             # We generate the list of architectures to build for by simply checking which architectures the compiler supports.
             # Mac OS X 10.7 does not support building for ppc _at all_.
             # NOTE: that $ARCHS is reassigned next-
-    export ARCHFLAGS="-arch_errors_fatal $(for arch in ${ARCHS:=x86_64 i386}; do printf -- "-arch $arch "; done)"
+#    export ARCHFLAGS="-arch_errors_fatal $(for arch in ${ARCHS:=x86_64 i386}; do printf -- "-arch $arch "; done)"
         # Don't let "missing arch" or such get by.
 
     #OBJCXFLAGS: Flags to use for _either_ Obj-C _or_ Obj-C++, see OBJCFLAGS and OBJCXXFLAGS below.
-    OBJCXFLAGS+="${OBJCXFLAGS:+ }-fobjc-gc" # -fzero-link
+#    OBJCXFLAGS+="${OBJCXFLAGS:+ }-fobjc-gc" # -fzero-link
         # Enable garbage collection support.
-    export LDFLAGS+="${LDFLAGS:+ }-pie"
+#    export LDFLAGS+="${LDFLAGS:+ }-pie"
         # Produce a position independent executable, assumes -fPIC (default on Darwin).
 fi
 
 # Convenience variables.
-DEBUGFLAGS+="${DEBUGFLAGS:+ }-gstabs+"
+#DEBUGFLAGS+="${DEBUGFLAGS:+ }-gstabs+"
     # generate debugging info, but only store it _inside_ the object (don't generate an external dSYM bundle).
 OPTFLAGS+="${OPTFLAGS:+ }-Os" 
     # Optimize for size, but not at the expense of speed. 
