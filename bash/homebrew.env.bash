3393dbaeaefd68f089ab4508159db92358dfede2
John D Pell <John+bashd@gaelicWizard.net>
2014-06-27 15:32:03 -0700

    Add defaults for `brew cask`: use /Applications instead of ~/Applications.

---
+++ bash/homebrew.env.bash
@@ -0,0 +1,6 @@
+#!/bin/bash --fail
+
+# Specify defaults for Homebrew Cask.
+# (Install cask with brew tap caskroom/cask and brew tap caskroom/versions.)
+export HOMEBREW_CASK_OPTS="--appdir=/Applications" # Use global /Applications instead of ~/Applications.
+
