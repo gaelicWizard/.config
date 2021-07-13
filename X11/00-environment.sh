dce4005cd057dbb4dbbad2e10f85f9e545ebf724
John D Pell <John+git@gaelicWizard.net>
2021-07-12 23:57:25 -0700

    GNUSTEP_USER_LIBRARY unset for WindowMaker

--- X11/00-environment.sh
+++ X11/00-environment.sh
@@ -4,14 +4,16 @@
 . /Library/GNUstep/Makefiles/GNUstep.sh
 #export WMAKER_USER_ROOT=~
 : "${GNUSTEP_USER_DEFAULTS_DIR:=$(gnustep-config --variable=GNUSTEP_USER_DEFAULTS_DIR)}"
-: "${GNUSTEP_USER_LIBRARY:=$(gnustep-config --variable=GNUSTEP_USER_LIBRARY)}"
+#: "${GNUSTEP_USER_LIBRARY:=$(gnustep-config --variable=GNUSTEP_USER_LIBRARY)}"
 : "${USERSH:=$(defaults read "${X11_PREFS_DOMAIN}" login_shell 2>/dev/null)}"
 : "${USERWM:=$(defaults read "${X11_PREFS_DOMAIN}" wm_path 2>/dev/null)}"
 
-if [ -n "${GNUSTEP_USER_DEFAULTS_DIR:=Library/Preferences}" ]
+if [ -d "${GNUSTEP_USER_DEFAULTS_DIR:=Library/Preferences}" ]
 then
 	export GNUSTEP_USER_DEFAULTS_DIR
 fi
+
+unset GNUSTEP_USER_LIBRARY # arg
 
 if [ -n "${USERSH:-}" ]
 then
