1752f5f08fa8a45b4611941c7ff5d6aeb6d79be5
John D Pell <John+git@gaelicWizard.net>
2021-07-30 00:56:16 -0700

    local: can only be used in a function

--- Bash/ssh.env.bash
+++ Bash/ssh.env.bash
@@ -1,15 +1,15 @@
 #!/bin/bash --fail
 
+unset SSH_HOSTNAME SSH_CLIENT
+
 # Figure out the incoming hostname for ssh connections
 #  I do this because SSH_CONNECTION is preserved by my environment,
 #  whereas SSH_HOSTNAME is not (since it is redundant).
-unset SSH_HOSTNAME
 if [ -n "${SSH_CONNECTION:-}" ]
 then 
-    local i
     for i in `host $(echo $SSH_CONNECTION | awk '{print $1}')`; do SSH_HOSTNAME="$i"; done
     export SSH_HOSTNAME
-fi
+fi; unset i
 
 # Reset SSH_CLIENT from SSH_CONNECTION
 #  I do this because SSH_CONNECTION is preserved by my environment,
