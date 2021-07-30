c7aa1e217492a274b4ebb188ebec4ca8f6f13c1d
John D Pell <John+git@gaelicWizard.net>
2021-07-30 01:01:52 -0700

    Rewrite to account for missing `host`

--- Bash/ssh.env.bash
+++ Bash/ssh.env.bash
@@ -1,20 +1,22 @@
 #!/bin/bash --fail
 
+##
+# Clear variables expected to be inaccurate.
 unset SSH_HOSTNAME SSH_CLIENT
+#
 
-# Figure out the incoming hostname for ssh connections
-#  I do this because SSH_CONNECTION is preserved by my environment,
-#  whereas SSH_HOSTNAME is not (since it is redundant).
+# Reconstruct SSH_HOSTNAME from SSH_CONNECTION.
 if [ -n "${SSH_CONNECTION:-}" ]
 then 
-    for i in `host $(echo $SSH_CONNECTION | awk '{print $1}')`; do SSH_HOSTNAME="$i"; done
-    export SSH_HOSTNAME
+	for i in "${SSH_CONNECTION%% *}" "$(host ${SSH_CONNECTION%% *} 2>/dev/null)"
+	do
+		SSH_HOSTNAME="$i"
+	done
+	export SSH_HOSTNAME
 fi; unset i
 
-# Reset SSH_CLIENT from SSH_CONNECTION
-#  I do this because SSH_CONNECTION is preserved by my environment,
-#  whereas SSH_CLIENT is not (since it is redundant).
+# Reconstruct SSH_CLIENT from SSH_CONNECTION
 if [ -n "${SSH_CONNECTION:-}" ]
 then
-    export SSH_CLIENT=`echo "$SSH_CONNECTION" | awk '{print $1 " " $2 " " $4}'`
+	export SSH_CLIENT=`echo "$SSH_CONNECTION" | awk '{print $1 " " $2 " " $4}'`
 fi
