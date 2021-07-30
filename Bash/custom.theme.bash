2426d6ec180b85d1ff928e3aa75c32c83cc903c4
John D Pell <John+git@gaelicWizard.net>
2021-07-30 02:03:20 -0700

    Adopt _Bash It_'s convenience variables for my \[\e[G\][\033[0;33m\h\033[0m:\w] \[\033[0;34m\]\u\[\033[0m\]\[`[ $? -ne 0 ] && echo -n '\033[0;31m'`\]\$\[\033[0m\]

--- Bash/custom.theme.bash
+++ Bash/custom.theme.bash
@@ -2,10 +2,9 @@
 
 ##
 if shell_is_interactive
-then
-    PS1="\[\e[G\][\h:\w\$(oy=$?; __git_ps1 \" (%s)\" 2>/dev/null; exit \$oy)] \[\e[1;34m\]\u\[\e[0m\]\`[ \$? -ne 0 ] && echo -n '\[\e[00;31m\]'\`\\$\[\e[0m\] "
+then #\$(oy=$?; __git_ps1 \" (%s)\" 2>/dev/null; exit \$oy)
+	PS1="\[\e[G\][${echo_yellow}\h${echo_normal}:\w] \[${echo_blue:-}\]\u\[${echo_normal:-}\]\[\`[ \$? -ne 0 ] && echo -n '${echo_red:-}'\`\]\\$\[${echo_normal}\] "
 fi
-        # My prompt line: 	"[hostname:~/Documents] <span style="font-color:blue;font-weight:bold;">user</span>$ " ('#' instead of '$', if root) ('$' becomes <span style="font-color:red;">$</span> when previous command failed)
-        # Current git branch will appear after current directory, if appropriate.
-        # Prompt will _always_ start at the beginning of the line.
+# My prompt line: 	"[hostname:~/Documents] <span style="font-color:blue;font-weight:bold;">user</span>$ " ('#' instead of '$', if root) ('$' becomes <span style="font-color:red;">$</span> when previous command failed)
+# Prompt will _always_ start at the beginning of the line.
 ##
