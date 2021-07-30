78979da4c8be65051e59437ab6db11c12941cb5b
John D Pell <John+git@gaelicWizard.net>
2021-07-30 01:10:10 -0700

    Improve logic and looping

--- Bash/ssh.env.bash
+++ Bash/ssh.env.bash
@@ -5,18 +5,12 @@
 unset SSH_HOSTNAME SSH_CLIENT
 #
 
-# Reconstruct SSH_HOSTNAME from SSH_CONNECTION.
+# Reconstruct SSH_HOSTNAME and SSH_CLIENT from SSH_CONNECTION.
 if [ -n "${SSH_CONNECTION:-}" ]
 then 
-	for i in "${SSH_CONNECTION%% *}" "$(host ${SSH_CONNECTION%% *} 2>/dev/null)"
+	for i in ${SSH_CONNECTION%% *} $(host ${SSH_CONNECTION%% *} 2>/dev/null)
 	do
-		SSH_HOSTNAME="$i"
-	done
-	export SSH_HOSTNAME
-fi; unset i
-
-# Reconstruct SSH_CLIENT from SSH_CONNECTION
-if [ -n "${SSH_CONNECTION:-}" ]
-then
+		export SSH_HOSTNAME="$i"
+	done; unset i
 	export SSH_CLIENT=`echo "$SSH_CONNECTION" | awk '{print $1 " " $2 " " $4}'`
 fi
