ecaaf4794dc9a0de918c3a6a7d14e684aa8ba35f
John D Pell <John+git@gaelicWizard.net>
2021-07-30 00:17:53 -0700

    Hack PS1 to not fail if __git_prompt doesn't exist

--- Bash/custom.theme.bash
+++ Bash/custom.theme.bash
@@ -3,7 +3,7 @@
 ##
 if shell_is_interactive
 then
-    PS1="\[\e[G\][\h:\w\$(__git_ps1 \" (%s)\")] \[\e[1;34m\]\u\[\e[0m\]\`[ \$? -ne 0 ] && echo -n '\[\e[00;31m\]'\`\\$\[\e[0m\] "
+    PS1="\[\e[G\][\h:\w\$(oy=$?; __git_ps1 \" (%s)\" 2>/dev/null; exit \$oy)] \[\e[1;34m\]\u\[\e[0m\]\`[ \$? -ne 0 ] && echo -n '\[\e[00;31m\]'\`\\$\[\e[0m\] "
 fi
         # My prompt line: 	"[hostname:~/Documents] <span style="font-color:blue;font-weight:bold;">user</span>$ " ('#' instead of '$', if root) ('$' becomes <span style="font-color:red;">$</span> when previous command failed)
         # Current git branch will appear after current directory, if appropriate.
