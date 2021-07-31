0886d325a804f307c31d697a51069eae3e77a921
John D Pell <John+git@gaelicWizard.net>
2021-07-30 22:27:49 -0700

    Use $XDG_STATE_HOME instead of ~/.temp

--- Bash/roulette.rc.bash
+++ Bash/roulette.rc.bash
@@ -1,6 +1,6 @@
 roulette () 
 { 
-    local teh_file=~/.temp/Untitled.txt;
+    local teh_file="${XDG_STATE_HOME}/Untitled.txt";
     local teh_list=($( < "$teh_file" ));
     local teh_winner="$(( $RANDOM % ${#teh_list[@]} ))";
     open "${teh_list[$teh_winner]}" && perl -pi -e "s;^${teh_list[$teh_winner]};;" "$teh_file"
