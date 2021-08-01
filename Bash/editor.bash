a7b4183a64a67827a033ac23fe3a871cb5261717
John D Pell <John+git@gaelicWizard.net>
2021-07-31 22:13:19 -0700

    Expand variable usage

--- Bash/editor.bash
+++ Bash/editor.bash
@@ -1,5 +1,9 @@
 #!/bin/bash --fail
 
+: ${VISUAL:=mate_wait}
+: ${EDITOR:=emacsclient}
+: ${ALTERNATE_EDITOR:=nano}
+
 #: "${EDITOR:=~/Tools/mate_wait}" "${ALTERNATE_EDITOR:=/usr/bin/nano}"
 
 alias e='${EDITOR:-nano}' # simpler, shorter, better.
