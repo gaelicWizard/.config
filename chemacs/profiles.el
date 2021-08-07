daa317507fad515bed53686464aeb189fe7c7fd5
John D Pell <John+git@gaelicWizard.net>
2021-08-07 05:45:56 -0400

    Add `Aquamacs` profile

--- chemacs/profiles.el
+++ chemacs/profiles.el
@@ -1,10 +1,9 @@
-(("spacemacs" . ((user-emacs-directory . "~/Library/.ApplicationSupport/Spacemacs")
+(("gnu.emacs" . ((user-emacs-directory . "~/Library/Preferences/GNU Emacs")))
+
+ ("Aquamacs" . ((user-emacs-directory . "~/Library/Preferences/Aquamacs Emacs")))
+
+ ("spacemacs" . ((user-emacs-directory . "~/Library/.ApplicationSupport/Spacemacs")
                  (env . (("SPACEMACSDIR" . "~/Library/Preferences/Spacemacs")))))
-
- ("spacemacs-develop" . ((user-emacs-directory . "~/Library/.ApplicationSupport/")
-                        (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
-
- ("gnu.emacs" . ((user-emacs-directory . "~/Library/Preferences/GNU Emacs")))
 
  ("doom" . ((user-emacs-directory . "~/Library/Application Support/Doom Emacs")
            (env . (("DOOMDIR" . "~/Library/Preferences/Doom Emacs")))))
