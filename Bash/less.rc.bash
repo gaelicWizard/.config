927913cc4465ed962d161b092454a834f8ebbd3e
John D Pell <John+git@gaelicWizard.net>
2021-09-04 14:23:41 -0700

    Specify some defaults for `less`...unclear how this relates to `lesskey`...

--- Bash/less.rc.bash
+++ Bash/less.rc.bash
@@ -4,3 +4,9 @@ if type -p lesspipe.sh >/dev/null
 then
 	export LESSOPEN="|lesspipe.sh %s" LESS_ADVANCED_PREPROCESSOR=1
 fi
+
+if type -p less >/dev/null
+then
+	export LESS='Ris'
+	# Show color, match patterns case-insensitively, and deduplicate consequtive blank lines.
+fi
