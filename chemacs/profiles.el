61a223cc21998998dc999fe08e80de60ab2815e7
John D Pell <John+git@gaelicWizard.net>
2021-09-04 14:56:42 -0700

    Install `chemacs2` profile manager for Emacs

---
+++ chemacs/profiles.el
@@ -0,0 +1,11 @@
+(("spacemacs" . ((user-emacs-directory . "~/Library/.ApplicationSupport/Spacemacs")
+                 (env . (("SPACEMACSDIR" . "~/Library/Preferences/Spacemacs")))))
+
+ ("spacemacs-develop" . ((user-emacs-directory . "~/Library/.ApplicationSupport/")
+                        (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
+
+ ("gnu.emacs" . ((user-emacs-directory . "~/Library/Preferences/GNU Emacs")))
+
+ ("doom" . ((user-emacs-directory . "~/Library/Application Support/Doom Emacs")
+           (env . (("DOOMDIR" . "~/Library/Preferences/Doom Emacs")))))
+)
