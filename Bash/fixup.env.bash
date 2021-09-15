53a97a628fd08d8c2aa67910602c463b49e743a7
John D Pell <John+git@gaelicWizard.net>
2021-09-15 14:52:18 -0700

    For newer versions of _Bash_, make sure that variable is tested for existence before using for parameter substitution.

--- Bash/fixup.env.bash
+++ Bash/fixup.env.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Adjust variables to be shell-friendly...because nobody can properly quote anything...
 
-[[ "${GNUSTEP_USER_APPLICATION_SUPPORT:-}" != "${GNUSTEP_USER_APPLICATION_SUPPORT/Application Support/.ApplicationSupport}" ]] \
+[[ -n "${GNUSTEP_USER_APPLICATION_SUPPORT:-}" && "${GNUSTEP_USER_APPLICATION_SUPPORT:-}" != "${GNUSTEP_USER_APPLICATION_SUPPORT/Application Support/.ApplicationSupport}" ]] \
  && [[ -d "${GNUSTEP_USER_APPLICATION_SUPPORT/Application Support/.ApplicationSupport}" ]] \
   && GNUSTEP_USER_APPLICATION_SUPPORT="${GNUSTEP_USER_APPLICATION_SUPPORT/Application Support/.ApplicationSupport}"
 
-[[ "${XDG_DATA_HOME:-}" != "${XDG_DATA_HOME/Application Support/.ApplicationSupport}" ]] \
+[[ -n  "${XDG_DATA_HOME:-}" && "${XDG_DATA_HOME:-}" != "${XDG_DATA_HOME/Application Support/.ApplicationSupport}" ]] \
  && [[ -d "${XDG_DATA_HOME/Application Support/.ApplicationSupport}" ]] \
   && XDG_DATA_HOME="${XDG_DATA_HOME/Application Support/.ApplicationSupport}"
 
-[[ "${XDG_STATE_HOME:-}" != "${XDG_STATE_HOME/Saved Application State/.savedState}" ]] \
+[[ -n "${XDG_STATE_HOME:-}" && "${XDG_STATE_HOME:-}" != "${XDG_STATE_HOME/Saved Application State/.savedState}" ]] \
  && [[ -d "${XDG_STATE_HOME/Saved Application State/.savedState}" ]] \
   && XDG_STATE_HOME="${XDG_STATE_HOME/Saved Application State/.savedState}"
 
