af7609d83fbcc5805e262cb8993ec2dd7ee48567
John D Pell <John+git@gaelicWizard.net>
2021-10-05 00:23:12 -0700

    Use `[[`, still needs work. This may need some significant work to remove dependence on word splitting. Also, `awk`?

--- Bash/ssh.env.bash
+++ Bash/ssh.env.bash
@@ -6,11 +6,11 @@ unset SSH_HOSTNAME SSH_CLIENT
 #
 
 # Reconstruct SSH_HOSTNAME and SSH_CLIENT from SSH_CONNECTION.
-if [ -n "${SSH_CONNECTION:-}" ]
+if [[ -n "${SSH_CONNECTION:-}" ]]
 then 
-	for i in ${SSH_CONNECTION%% *} $(host ${SSH_CONNECTION%% *} 2>/dev/null)
+	for i in ${SSH_CONNECTION%% *} $(host "${SSH_CONNECTION%% *}" 2>/dev/null)
 	do
 		export SSH_HOSTNAME="$i"
 	done; unset i
-	export SSH_CLIENT=`echo "$SSH_CONNECTION" | awk '{print $1 " " $2 " " $4}'`
+	export SSH_CLIENT="$(echo "$SSH_CONNECTION" | awk '{print $1 " " $2 " " $4}')"
 fi
