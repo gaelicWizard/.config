8d83f0f1aa847a0b95119684d35b5b366c04a867
John D Pell <John+git@gaelicWizard.net>
2021-10-05 00:28:31 -0700

    Adapt to new _Bash It_ changes, also disable completely pending re-write.

--- Bash/git_prompt.rc.bash
+++ Bash/git_prompt.rc.bash
@@ -1,5 +1,9 @@
+# shellcheck shell=bash
+
+return # disable for now
+
 : ${GIT_PROMPT_FETCH_REMOTE_STATUS:=} ${GIT_PROMPT_SHOW_UNTRACKED_FILES:=} ${GIT_PROMPT_IGNORE_SUBMODULES:=}
-gp_install_prompt () 
+function gp_install_prompt()
 { 
 	[[ -z "${OLD_GITPROMPT+x}" ]] && OLD_GITPROMPT="${PS1}"
 	[[ -z "${GIT_PROMPT_OLD_DIR_WAS_GIT+x}" ]] && GIT_PROMPT_OLD_DIR_WAS_GIT="$(we_are_on_repo)"
@@ -18,7 +22,6 @@ then
 	GIT_PROMPT_ONLY_IN_REPO=1
 	_bash_it_log_section="git-prompt"
 	source "${__GIT_PROMPT_DIR}/gitprompt.sh" 2> >(fgrep -v 'gp_install_prompt: readonly')
-	_log_trace "${__bash_it_log_prefix[0]:-} + ${__bash_it_log_prefix[1]:-}"
 	[[ "${__bash_it_log_prefix[0]:-zero}" == "git-prompt" && "${__bash_it_log_prefix[1]:-one}" == "git_prompt.rc" ]] \
 		&& _bash_it_log_prefix_pop "Sourcing gitprompt.sh fails part-way through and doesn't trigger RETURN trap..."
 fi
