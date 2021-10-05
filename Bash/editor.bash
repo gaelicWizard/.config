7008b99de5111ff7cced9327cb11dbea3bc82e82
John D Pell <John+git@gaelicWizard.net>
2021-10-05 00:20:58 -0700

    Quote `$EDITOR` (et al), also use all available variations in `e` alias.

--- Bash/editor.bash
+++ Bash/editor.bash
@@ -1,9 +1,10 @@
 #!/bin/bash --fail
 
-: ${VISUAL:=mate_wait}
-: ${EDITOR:=emacsclient}
-: ${ALTERNATE_EDITOR:=nano}
+: "${VISUAL:=mate_wait}"
+: "${EDITOR:=emacsclient}"
+: "${ALTERNATE_EDITOR:=nano}"
 
 #: "${EDITOR:=~/Tools/mate_wait}" "${ALTERNATE_EDITOR:=/usr/bin/nano}"
 
-alias e='${EDITOR:-nano}' # simpler, shorter, better.
+alias e='${VISUAL:-${EDITOR:-${ALTERNATE_EDITOR:-pico}}}' # simpler, shorter, better.
+	# If we're in a situation where everything is missing, then `pico` is slightly more likely to be installed.
