bff313479048a5a2cba91f1b0647c49d1fa9ed49
John D Pell <John+git@gaelicWizard.net>
2021-10-05 00:22:04 -0700

    Whitespace

--- Bash/opt.env.bash
+++ Bash/opt.env.bash
@@ -1,33 +1,30 @@
 #!/bin/bash
 
 declare -F pathmunge >/dev/null || { echo "opt: Unable to manipulate path." 1>&2; return; }
-    # Require my path package
-        #bashd_add_to_path_back()
-        #bashd_add_to_path_back_if_admin()
 
 for optPack in /opt/*
 do
-    if [ "$optPack" == "/opt/macports" ] || [ "$optPack" == "/opt/local" ]
-    then continue # Ignore MacPorts.
-    fi
+	if [ "$optPack" == "/opt/macports" ] || [ "$optPack" == "/opt/local" ]
+	then continue # Ignore MacPorts.
+	fi
 
 	if [ -d "$optPack" ]
-        then
+		then
 		if [ -d $optPack/bin ]
-        then
+		then
 			pathmunge "$optPack/bin" after
 		fi
 		if [ -d $optPack/sbin ]
-        then
+		then
 			pathmunge "$optPack/sbin" after
 		fi
 		if [ -d $optPack/share/aclocal ]
-        then
+		then
 			export ACLOCAL_FLAGS="-I $optPack/share/aclocal${ACLOCAL_FLAGS:+ }${ACLOCAL_FLAGS:-}"
 			#TODO:FIXME: $ACLOCAL_FLAGS is invalid in the event of a space in $optPack.
 		fi
 		if [ -d $optPack/lib/pkgconfig ]
-        then
+		then
 			export PKG_CONFIG_PATH="$optPack/lib/pkgconfig/${PKG_CONFIG_PATH:+:}${PKG_CONFIG_PATH:-}"
 		fi
 	fi
