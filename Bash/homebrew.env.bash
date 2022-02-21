5e83d306370f0045c6390cc363426c6764227abd
John D Pell <John+Git@gaelicWizard.net>
2022-02-20 20:39:23 -0800

    Set not to update but once in 24-hours.

--- Bash/homebrew.env.bash
+++ Bash/homebrew.env.bash
@@ -1,4 +1,6 @@
-#!/bin/bash -c 'brew'
+# shellcheck shell=bash
+
+export HOMEBREW_AUTO_UPDATE_SECS="86400" # 24-hours
 
 # Specify defaults for Homebrew Cask.
 # (Install cask with brew tap caskroom/cask and brew tap caskroom/versions.)
@@ -19,9 +21,8 @@ export HOMEBREW_CASK_OPTS="
 --internet-plugindir=/Library/Internet\ Plug-Ins
 --screen-saverdir=/Library/Screen\ Savers
 "
-	# Use global /Applications instead of ~/Applications.
+# Use global /Applications instead of ~/Applications.
 
-if _bash_it_homebrew_check && [[ "${BASH_VERSINFO[0]}" -ge "4" && -r "${HB_CNF_HANDLER:=${BASH_IT_HOMEBREW_PREFIX}/handler.sh}" ]]
-then
+if _bash_it_homebrew_check && [[ "${BASH_VERSINFO[0]}" -ge "4" && -r "${HB_CNF_HANDLER:=${BASH_IT_HOMEBREW_PREFIX}/handler.sh}" ]]; then
 	source "$HB_CNF_HANDLER"
 fi
