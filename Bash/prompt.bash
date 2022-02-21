2e48ff2e8749c517127c1499ee5a7014af84235d
John D Pell <John+Git@gaelicWizard.net>
2022-02-20 20:14:39 -0800

    Set $PROMPT rather than $PS1 directly, to play nice with _Bash It_ themes that set $PS1 via $PROMPT_COMMAND.

--- Bash/prompt.bash
+++ Bash/prompt.bash
@@ -1,9 +1,10 @@
-#!/bin/bash --interactive
+# shellcheck shell=bash
 
 ##
 if shell_is_interactive
 then #\$(oy=$?; __git_ps1 \" (%s)\" 2>/dev/null; exit \$oy)
-	PS1="\[\e[G\][\[${echo_yellow:-}\]\h\[${echo_normal:-}\]:\w] \[${echo_blue:-}\]\u\[${echo_normal:-}\]\[\`[ \$? -ne 0 ] && echo -n '${echo_red:-}'\`\]\\$\[${echo_normal:-}\] "
+	PS0="${normal:-}\[\e]7;file://\h/\${PWD}\a\]"
+	PROMPT="\[\e[G\]${normal:-}[${yellow:-}\h${normal:-}:\w v\v] ${blue:-}\u${normal:-}\`[[ \$? -ne 0 ]] && printf '${red:-}'\`\\\$${normal:-} "
 fi
 # My prompt line: 	"[hostname:~/Documents] <span style="font-color:blue;font-weight:bold;">user</span>$ " ('#' instead of '$', if root) ('$' becomes <span style="font-color:red;">$</span> when previous command failed)
 # Prompt will _always_ start at the beginning of the line.
